6fd398a4b52530d1b5b85b2fe97f1b20c39a5ebe
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-17 13:03:03 +0300

    fix fetch

--- src/js/news-servise.js
+++ src/js/news-servise.js
@@ -11,7 +11,7 @@ export default class NewsApiService {
   fetchArticles() {
     const KEY_AUTH = '30593721-3615c14b1fd526cc46c7cd9ff';
     console.log('class', this.searchQuery);
-    const url = `https://pixabay.com/api/?key=${KEY_AUTH}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=4`;
+    const url = `https://pixabay.com/api/?key=${KEY_AUTH}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
 
     return axios.get(url).then(res => {
       this.page += 1;
